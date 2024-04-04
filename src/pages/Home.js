5967258b12238eaaa6e2af16f3a884c8725b34a3
ksaiteja <33175083+ksaiteja@users.noreply.github.com>
2024-04-04 14:59:39 +0530

    tailwing-config

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="main-container flex flex-row h-screen w-screen">
+      <div className="chats-container h-screen flex w-1/2 bg-black"></div>
+    </div>
+  );
 };
 
 export default Home;
